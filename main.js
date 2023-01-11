1f17f3c5fa358aad66bfb6074586a07a17513ff5
João Edson Dantas <joaoedson.dantas@outlook.com>
2023-01-11 11:34:21 -0300

    Adicionado o modal para adiconar as tarefas

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-const caixasInput = document.querySelectorAll(".check-item input");
-const caixas = document.querySelectorAll(".check-item");
-const teste = document.querySelectorAll(".check-body");
+function initTasks() {
+  const caixasInput1 = document.querySelectorAll(".check-item input");
+  const completedTasks = document.querySelector(".completedTasks");
+  const allTasks = document.querySelector(".allTasks");
 
-function addList(event) {
-  const elementPai = this.parentElement;
-  elementPai.classList.toggle("checked");
+  function addListCompleted() {
+    const elementPai = this.parentElement;
+    elementPai.classList.toggle("checked");
+    completedTasks.appendChild(elementPai);
+    if (!elementPai.classList.contains("checked")) {
+      allTasks.appendChild(elementPai);
+    }
+  }
+
+  caixasInput1.forEach((item) => {
+    item.addEventListener("click", addListCompleted);
+  });
+}
+initTasks();
+
+function initModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    modal.classList.add("modal-ativo");
+    modal.addEventListener("click", (event) => {
+      if (event.target.id == modalId || event.target.id == "fechar-modal") {
+        modal.classList.remove("modal-ativo");
+      }
+    });
+  }
 }
 
-caixasInput.forEach((item) => {
-  item.addEventListener("click", addList);
-});
+const btnModal = document.querySelector(".add-task");
+btnModal.addEventListener("click", () => initModal("modalAdd"));
